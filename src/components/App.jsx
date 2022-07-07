a809314a348bbf503f89374249a64c29b3869759
Kaijapen <104542328+Kaijapen@users.noreply.github.com>
2022-07-06 19:50:16 -0700

    commit these pls

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './App.css';
+
+
+
+class App extends Component {
+    render(){
+        return(
+            <div className="App">
+                <h1>Hello Kaija (●'◡'●) ❤️</h1>
+                <h2>Things I need to do:</h2>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Feed the dogs</li>
+                    <li>Run a marathon</li>
+                </ul>
+            </div>
+
+        );
+    }
+}
+
+export default App;
